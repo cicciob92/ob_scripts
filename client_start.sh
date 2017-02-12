cc276dd700d33dffdf306673e665622301adc244
francesco benforte <cicciob92@gmail.com>
2017-02-12 16:28:48 +0100

    changes

--- client_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-screen -d -m -S client ping -c 4 $server_private
--- firewall_configure.sh
+++ firewall_configure.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-user_side_port="$(ifconfig | grep -B 1 $server_private | head -1 | awk {'print($1)'})"
-wan_side_port="$(ifconfig | grep -B 1 $server_altra | head -1 | awk {'print($1)'})"
-
-echo "user_side_port: $user_side_port"
-echo "wan_side_port: $wan_side_port"
-
-echo "route - output command:"
-route
-
-iptables -t nat -A POSTROUTING -o $wan_side_port -j MASQUERADE
-
+ping -c 4 $firewall_private
