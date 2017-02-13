0d497d075821fa87a519aa1a6317f68070d38d8e
Francesco Benforte <cicciob92@gmail.com>
2017-02-13 12:16:53 +0100

    Update firewall_configure.sh

--- firewall_configure.sh
+++ firewall_configure.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo route del default
-sudo route add default gw $firewall_private
-echo "Displaying routing table:"
-route -n
+#sudo route del default
+#sudo route add default gw $firewall_private
+
+echo "executing command: route add -host 130.192.225.254 gw $firewall_private"
+route add -host 130.192.225.254 gw $firewall_private
+
+#echo "Displaying routing table:"
+#route -n
