82ff0ee86e00b3ad8fafa2b17f2dfa7592192d62
Francesco Benforte <cicciob92@gmail.com>
2017-02-13 13:43:09 +0100

    Update firewall_setup.sh

--- firewall_setup.sh
+++ firewall_setup.sh
@@ -2,14 +2,18 @@
 
 user_side_port="$(ifconfig | grep -B 1 $private | head -1 | awk {'print($1)'})"
 wan_side_port="$(ifconfig | grep -B 1 $altra | head -1 | awk {'print($1)'})"
+default_gw="$(route -n | grep "UG" | head -1 | awk {'print($2)'})"
+broker_ip="$(cat /etc/openbaton/ems/conf.ini | grep broker_ip | awk -F "=" {'print($2)'})"
 
 echo "user_side_port: $user_side_port"
 echo "wan_side_port: $wan_side_port"
 
-#echo "executing command: sudo route del default"
-#sudo route del default
-#echo "executing command: sudo dhclient -v $wan_side_port"
-#sudo dhclient -v $wan_side_port
+echo "executing command: sudo route add -host $broker_ip gw $default_gw"
+sudo route add -host $broker_ip gw $default_gw
+echo "executing command: sudo route del default"
+sudo route del default
+echo "executing command: sudo dhclient -v $wan_side_port"
+sudo dhclient -v $wan_side_port
 
 echo "executing command: iptables -t nat -A POSTROUTING -o $wan_side_port -j MASQUERADE"
 iptables -t nat -A POSTROUTING -o $wan_side_port -j MASQUERADE
