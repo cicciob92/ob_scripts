b413158188dc068173dc691d1de7979721f92588
Francesco Benforte <cicciob92@gmail.com>
2017-02-13 11:47:34 +0100

    Update firewall_setup.sh

--- firewall_setup.sh
+++ firewall_setup.sh
@@ -6,8 +6,13 @@ wan_side_port="$(ifconfig | grep -B 1 $altra | head -1 | awk {'print($1)'})"
 echo "user_side_port: $user_side_port"
 echo "wan_side_port: $wan_side_port"
 
-echo "route - output command:"
-route
+echo "executing command: sudo route del default"
+sudo route del default
+echo "executing command: sudo dhclient -v $wan_side_port"
+sudo dhclient -v $wan_side_port
 
+echo "executing command: iptables -t nat -A POSTROUTING -o $wan_side_port -j MASQUERADE"
 iptables -t nat -A POSTROUTING -o $wan_side_port -j MASQUERADE
 
+echo "Displaying routing table:"
+route
