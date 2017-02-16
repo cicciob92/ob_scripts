dd9e1adb3e2cd7a7f59c736657540a926b7a7b5c
francesco benforte <cicciob92@gmail.com>
2017-02-16 10:17:42 +0100

    new conf files for ipsec peers

---
+++ ipsec_peer1_configure.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+user_side_port="$(ifconfig | grep -B 1 $private2 | head -1 | awk {'print($1)'})"
+wan_side_port="$(ifconfig | grep -B 1 $altra | head -1 | awk {'print($1)'})"
+user_net="$(echo $private2 | awk -F "." '{OFS = ".";}{print $1,$2,$3,"0/24"}')"
+external_net="$(echo $altra | awk -F "." '{OFS = ".";}{print $1,$2,$3,"0/24"}')"
+
+#enable ipv4 forwarding
+sysctl -w net.ipv4.ip_forward=1
+
+#generate ipsec.conf
+
+echo "config setup
+   uniqueids = never
+
+conn %default
+   ikelifetime=60m
+   keylife=20m
+   rekeymargin=3m
+   keyingtries=1
+   authby=secret
+   keyexchange=ikev2
+   mobike=no
+
+conn net-net
+   left=$altra
+   leftid=@moon.strongswan.org
+   leftsubnet=$user_net
+   leftfirewall=yes
+   right=$ipsec_peer1_altra
+   rightsubnet=$external_net
+   rightid=@sun.strongswan.org
+   auto=start" > ipsec.conf
+
+
+ipsec start --conf ipsec.conf
+
